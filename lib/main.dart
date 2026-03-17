2a7e49bbd10ba69ae95e0ed4d0a43df02f220d6b
RONAKgoti <167706556+RONAKgoti@users.noreply.github.com>
2026-03-17 18:02:16 +0530

    Delete lib directory

--- lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'app.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  // Absolute Laws: Real Data Handling
-  runApp(const ProviderScope(child: WebApp()));
-}
