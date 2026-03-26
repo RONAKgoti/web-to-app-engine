9519bcf44f419d2bc30a2de2a9713301c19a4bb6
RONAKgoti <167706556+RONAKgoti@users.noreply.github.com>
2026-03-26 14:27:11 +0530

    Delete lib directory

--- lib/core/constants/app_constants.dart
+++
@@ -1,8 +0,0 @@
-class AppConstants {
-  static const String appName = "Expert WebFlow Pro";
-  static const double kPagePaddingMobile = 16.0;
-  static const double kPagePaddingTablet = 24.0;
-  static const double kCardRadius = 16.0;
-  static const double kButtonRadius = 12.0;
-  static const double kInputRadius = 12.0;
-}
